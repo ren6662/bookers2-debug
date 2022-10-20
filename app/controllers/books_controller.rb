469bd47d6eaf2417ff5d731cec733858c03baff2
ren6662 <114849966+ren6662@users.noreply.github.com>
2022-10-20 12:09:16 +0000

    [Finish]課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,14 @@
 class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @newbook = Book.new
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -12,7 +16,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
       @books = Book.all
       render 'index'
@@ -32,15 +36,22 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body, :image)
   end
+  
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
+  end
+  
 end
