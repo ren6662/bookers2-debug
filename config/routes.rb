d14cb65ae7d771b425305e4221b7f2ae9fde6723
ren6662 <114849966+ren6662@users.noreply.github.com>
2022-10-24 12:32:27 +0000

    [Add]検索機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'relationships/followers'
   get 'relationships/create'
   get 'relationships/destroy'
+  get "search" => "searches#search"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
